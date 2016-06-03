18536afbe8817128ea39af7b6fa7e0804193587a
Puneet Kr. Sharma, [Jr. Android Developer] @ TRANCIS <puneet.trancis@gmail.com>
2016-06-03 18:20:57 +0530

    Initial commit
    via Android Studio version- 2.1.1
    on 3rd June 2016, 6:20 PM
    
    by PUNEET at TRANCIS

---
+++ app/src/main/java/com/example/android/pushtogithub_as_v_211/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.android.pushtogithub_as_v_211;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * FIRST TIME CONTENT
+     *
+     * This file was create by Puneet at Trancis on 3rd June 2016
+     * via Android Studio Version- 2.1.1
+     * at 6:16 PM to upload at Github via Android Studio.
+     *
+     *
+     * */
+}
